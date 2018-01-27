e9b05ff4046bb86e76612af97f0096c1cfa0ff38
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-01-27 18:53:13 +0100

    Pause: Überlegen wie man Objekte vernünftig markiert...

---
+++ Assets/scripts/Obj_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_Controller : MonoBehaviour {
+
+    private List<GameObject> Obj_List = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject Temp;
+    public void GenerateObj(string name) {
+        
+        GameObject temp = (GameObject) Instantiate(Temp, this.transform);
+        Debug.Log("Generate OBJ " + temp.GetComponent<SpriteRenderer>().sprite.name);
+        temp.transform.position = new Vector3(temp.transform.position.x, Obj_List.Count + 1, temp.transform.position.z);
+        Obj_List.Add(temp);
+
+    }
+}
